219e64cf557532e8b1aaa94fddb06a0b3ec0d434
cupokki <nd1346@gmail.com>
2025-07-07 00:13:58 +0900

    init

---
+++ src/main/java/hackerthon/cchy/cchy25/Cchy25Application.java
@@ -0,0 +1,13 @@
+package hackerthon.cchy.cchy25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cchy25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cchy25Application.class, args);
+	}
+
+}
---
+++ src/test/java/hackerthon/cchy/cchy25/Cchy25ApplicationTests.java
@@ -0,0 +1,13 @@
+package hackerthon.cchy.cchy25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cchy25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
