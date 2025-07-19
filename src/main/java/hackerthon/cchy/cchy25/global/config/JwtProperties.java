09da335bd30f56d880afa1ce5badcaa183ec86bc
cupokki <nd1346@gmail.com>
2025-07-19 23:02:02 +0900

    fix: configuration properties 읽기 수정

--- src/main/java/hackerthon/cchy/cchy25/global/config/JwtProperties.java
+++ src/main/java/hackerthon/cchy/cchy25/global/config/JwtProperties.java
@@ -1,14 +1,18 @@
 package hackerthon.cchy.cchy25.global.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "jwt")
 @Getter
-@Component
+@AllArgsConstructor
 public class JwtProperties {
 
     private String secretKey;
     private Long accessTokenExp;
 }
+
