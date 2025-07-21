e71c896a3605e44c79afc66e56317c159e85e4d9
cupokki <nd1346@gmail.com>
2025-07-22 00:16:22 +0900

    feat: 소셜 회원가입 로직 추가

--- src/main/java/hackerthon/cchy/cchy25/domain/auth/service/AuthService.java
+++ src/main/java/hackerthon/cchy/cchy25/domain/auth/service/AuthService.java
@@ -1,31 +1,49 @@
 package hackerthon.cchy.cchy25.domain.auth.service;
 
-import hackerthon.cchy.cchy25.domain.auth.dto.JwtTokenDto;
-import hackerthon.cchy.cchy25.domain.auth.dto.UserLoginRequest;
-import hackerthon.cchy.cchy25.domain.auth.dto.UserSignUpRequest;
+import hackerthon.cchy.cchy25.domain.auth.dto.*;
+import hackerthon.cchy.cchy25.domain.auth.entity.SocialAccount;
 import hackerthon.cchy.cchy25.domain.auth.exception.AuthenticationErrorCode;
 import hackerthon.cchy.cchy25.domain.auth.exception.AuthenticationException;
 import hackerthon.cchy.cchy25.domain.auth.repository.AccessTokenBlackListRepository;
 import hackerthon.cchy.cchy25.domain.auth.repository.RefreshTokenWhitelistRepository;
+import hackerthon.cchy.cchy25.domain.auth.repository.SocialAccountRepository;
 import hackerthon.cchy.cchy25.domain.user.entity.User;
 import hackerthon.cchy.cchy25.domain.user.repository.UserRepository;
 import hackerthon.cchy.cchy25.global.security.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final SocialAccountRepository socialAccountRepository;
     private final AccessTokenBlackListRepository accessTokenBlackListRepository;
     private final RefreshTokenWhitelistRepository refreshTokenWhiteListRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
+    private final Map<SocialType, OauthService> oauthServiceStrategies;
+
+    public AuthService(UserRepository userRepository, SocialAccountRepository socialAccountRepository, AccessTokenBlackListRepository accessTokenBlackListRepository, RefreshTokenWhitelistRepository refreshTokenWhiteListRepository, JwtTokenProvider jwtTokenProvider, PasswordEncoder passwordEncoder, List<OauthService> oauthServices) {
+        this.userRepository = userRepository;
+        this.socialAccountRepository = socialAccountRepository;
+        this.accessTokenBlackListRepository = accessTokenBlackListRepository;
+        this.refreshTokenWhiteListRepository = refreshTokenWhiteListRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.passwordEncoder = passwordEncoder;
+        this.oauthServiceStrategies = oauthServices.stream()
+                .collect(Collectors.toMap(
+                        strategy -> strategy.getLoginType(),
+                        strategy -> strategy
+                ));
+    }
 
     @Transactional
     public JwtTokenDto login(UserLoginRequest userLoginRequest) {
@@ -48,30 +66,64 @@ public class AuthService {
         return jwtTokenDto;
     }
 
+    public JwtTokenDto socialLogin(UserLoginRequest userLoginRequest) {
+        var oauthService= oauthServiceStrategies.get(userLoginRequest.loginType());
+        var token = oauthService.getToken(userLoginRequest.code());
+        var socialId = oauthService.getSocialId(token);
+
+        var socialAccount = socialAccountRepository.findBySocialId(socialId);
+
+        // 소셜 계정을 찾을 수 없다면 회원가입
+        if (socialAccount.isEmpty()) {
+            return jwtTokenProvider.createSignupToken(socialId);
+        }
+
+        var foundUser = socialAccount.get().getUser();
+
+        return jwtTokenProvider.createToken(foundUser.getId(), false);
+    }
+
     @Transactional
     public JwtTokenDto signup(UserSignUpRequest userSignUpRequest) {
+
+        // 소셜 로그인 처리
+        if (userSignUpRequest.socialType() != SocialType.LOCAL && userSignUpRequest.signupToken() == null) {
+            throw new AuthenticationException(AuthenticationErrorCode.INVALID_CREDENTIALS); //TODO : 예외 수정
+        }
+
+        // 유저네임 증복 검증
         if (userRepository.existsByUsername(userSignUpRequest.username())) {
             throw new AuthenticationException(AuthenticationErrorCode.DUPLICATE_USERNAME);
         }
 
+        // 이메일 중복 검증
         if (userRepository.existsByEmail(userSignUpRequest.email())) {
             throw new AuthenticationException(AuthenticationErrorCode.DUPLICATE_EMAIL);
         }
 
+        // 확인 비밀번호 검증
         if (!userSignUpRequest.password().equals(userSignUpRequest.confirmPassword())) {
             throw new AuthenticationException(AuthenticationErrorCode.CONFIRM_PASSWORD_MISMATCH);
         }
 
-        var newUser = User.builder()
+        var newUser = userRepository.save(User.builder()
                 .username(userSignUpRequest.username())
                 .password(passwordEncoder.encode(userSignUpRequest.password()))
                 .email(userSignUpRequest.email())
-                .build();
+                .build());
 
-        userRepository.save(newUser);
+        if (userSignUpRequest.signupToken() != null) {
+            var socialAccount = socialAccountRepository.save(SocialAccount.builder()
+                    .socialId(userSignUpRequest.signupToken())
+                    .build());
+            newUser.addSocialAccount(socialAccount);
+        }
 
         return jwtTokenProvider.createToken(newUser.getId(), false);
     }
+
+
+
 //
 //    @Transactional
 //    public void logout(Long userId, String accessToken, String refreshToken) {
