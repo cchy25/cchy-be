4295596ccc11b1f27ee671dbb6b4c3491ecbf1d0
cupokki <nd1346@gmail.com>
2025-07-23 23:12:33 +0900

    feat: swagger 설정

--- src/main/java/hackerthon/cchy/cchy25/domain/user/controller/UserController.java
+++ src/main/java/hackerthon/cchy/cchy25/domain/user/controller/UserController.java
@@ -1,10 +1,11 @@
 package hackerthon.cchy.cchy25.domain.user.controller;
 
+import hackerthon.cchy.cchy25.domain.user.dto.UserResponse;
 import hackerthon.cchy.cchy25.domain.user.dto.UserUpdateRequest;
 import hackerthon.cchy.cchy25.domain.user.entity.User;
 import hackerthon.cchy.cchy25.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +17,17 @@ public class UserController {
 
     private final UserService userService;
 
+    @SecurityRequirement(name = "Authorization")
     @GetMapping("/user/me")
-    public ResponseEntity<?> me(
+    public ResponseEntity<UserResponse> me(
             @AuthenticationPrincipal User user
     ) {
         return ResponseEntity.ok(userService.me(user.getId()));
     }
 
+    @SecurityRequirement(name = "Authorization")
     @PutMapping("/user")
-    public ResponseEntity<?> updateUser(
+    public ResponseEntity<Void> updateUser(
             @AuthenticationPrincipal User user,
             @RequestBody UserUpdateRequest userUpdateRequest
     ) {
