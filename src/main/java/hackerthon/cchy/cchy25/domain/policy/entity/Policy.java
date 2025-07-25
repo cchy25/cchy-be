cc6eaef551c746104c6d799e1b9a54f25d51d550
cupokki <nd1346@gmail.com>
2025-07-25 22:59:46 +0900

    feat: policy 칼럼 추가

--- src/main/java/hackerthon/cchy/cchy25/domain/policy/entity/Policy.java
+++ src/main/java/hackerthon/cchy/cchy25/domain/policy/entity/Policy.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "policies")
 @Builder
@@ -24,4 +26,38 @@ public class Policy extends BaseEntity {
     private String title;
 
     private String summary;
+
+    private BusinessType businessType; // 사업 분야
+
+    private SupportType supportType; // 정책 종류
+
+    private String organization;
+
+    private LocalDateTime startAt;
+
+    private LocalDateTime endAt;
+
+    @Enumerated
+    private List<SupportTarget> targets;
+
+    @Enumerated
+    private List<EvaluationMethod> evaluationMethods;
+
+    private ApplicationMethod applicationMethod;
+
+//    private String currency;
+
+    private Long minAmount;
+
+    private Long maxAmount;
+
+    private Long isAlways;
+
+    private String condition;
+
+    private List<PolicyTag> tags = new ArrayList();
+
+
+
+
 }
