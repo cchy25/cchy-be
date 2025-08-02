f1b650f2f9aceb675d7185edb5182f1caefdea2d
Jung <nd1346@gmail.com>
2025-08-03 00:44:22 +0900

    feat: enum최신화

--- src/main/java/hackerthon/cchy/cchy25/domain/policy/entity/SupportCategory.java
+++ src/main/java/hackerthon/cchy/cchy25/domain/policy/entity/SupportCategory.java
@@ -8,8 +8,18 @@ import java.util.Arrays;
 @AllArgsConstructor
 @Getter
 public enum SupportCategory {
-    CONTEST("공모전/경진대회"),
-    SUPPORT("창업지원"),
+    FUNDING("재정지원"),
+    PROTOTYPE("시제품제작"),
+    SPACE("공간지원"),
+    TUTOR("교육"),
+    NETWORK("네트워킹"),
+    MENTORING("멘토링"),
+    TEAM("창업팀빌딩"),
+    ADMIN("행정지원"),
+    INVESTMENT("투자유치"),
+    MARKETING("마케팅/판로개척"),
+    HR("인력/고용"),
+    GLOBAL("글로벌"),
     ETC("기타");
 
     private String name;
--- src/main/java/hackerthon/cchy/cchy25/domain/policy/entity/SupportTarget.java
+++ src/main/java/hackerthon/cchy/cchy25/domain/policy/entity/SupportTarget.java
@@ -8,12 +8,13 @@ import java.util.Arrays;
 @AllArgsConstructor
 @Getter
 public enum SupportTarget {
-    SOLO("1인"),
+    SOLO("1인창업"),
     PRE("예비창업자"),
     RE("재창업"),
-    WOMEN("여성"),
+    WOMEN("여성기업"),
     UNIV("대학생"),
-    SOC("사회문제해결"),
+    SOC("사회적기업"),
+    SMBIZ("소상공인"),
     ETC("기타");
 
     private String name;
